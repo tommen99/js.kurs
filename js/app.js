6337ff444334edcf7cabf083c57521548140efc1
tom erik husås <tommen99@hotmail.com>
2018-03-21 14:35:33 +0100

    javascript

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var heading = document.querySelector('h1');
+var customheader = "something";
+heading.textContent = customheader;
+
+
+var number1 = 2;
+var number2 = 3;
+var text = "fancy shit";
+var myList = ["one", "two", "three"];
+var myObject = {}
+
+
+function multyplayBytwo(n1, n2){
+  var val = n2 + n1;
+  return val * 2;
+}
+
+function addtext(s1){
+  document.getElementById("textchange");
+  var retVal
+if (el.innerHTML === "rofl"){
+    el.innerHTML = null;
+}
+else{
+    el.innerHTML ="rofl"
+    }
+}
